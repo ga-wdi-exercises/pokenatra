2536a21a83fe53180b538165c6764dc2d1b4f370
Andy <andrew.sunglae.kim@gmail.com>
2016-07-25 13:36:31 -0400

    adds imgs

--- db/schema.sql
+++ db/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE pokemons(
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   cp INT NOT NULL,
-  poke_type TEXT NOT NULL
+  poke_type TEXT NOT NULL,
+  img_url TEXT NOT NULL
 )
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,11 @@ require_relative 'connection'
 require_relative '../models/pokemon'
 
 Pokemon.destroy_all
-types = ["physical", "magical", "fierce", "tomatoe", "pizza"]
-
-15.times do
-  Pokemon.create(name: FFaker::Name.name, poke_type: types.sample, cp: rand(800))
-end
+Pokemon.create(name: "Charmander", cp: rand(800), poke_type: "fire", img_url: "https://img.pokemondb.net/artwork/charmander.jpg")
+Pokemon.create(name: "Squirtle", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/squirtle.jpg")
+Pokemon.create(name: "Pickachu", cp: rand(800), poke_type: "lightning", img_url: "https://img.pokemondb.net/artwork/pikachu.jpg")
+Pokemon.create(name: "Bulbasaur", cp: rand(800), poke_type: "grass", img_url: "https://img.pokemondb.net/artwork/bulbasaur.jpg")
+Pokemon.create(name: "Clefairy", cp: rand(800), poke_type: "fairy", img_url: "https://img.pokemondb.net/artwork/clefairy.jpg")
+Pokemon.create(name: "Nidoking", cp: rand(800), poke_type: "poison ground", img_url: "https://img.pokemondb.net/artwork/nidoking.jpg")
+Pokemon.create(name: "Gengar", cp: rand(800), poke_type: "ghost poison", img_url: "https://img.pokemondb.net/artwork/gengar.jpg")
+Pokemon.create(name: "Blastoise", cp: rand(800), poke_type: "water", img_url: "https://img.pokemondb.net/artwork/blastoise.jpg")
